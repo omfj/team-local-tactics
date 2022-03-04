e0ae0e874c8205b213719eb1e977f0773b777342
Ole Magnus Johnsen <ole.magnus@me.com>
2022-03-04 14:25:52 +0100

    :zap: Better server.py

--- src/server/server.py
+++ src/server/server.py
@@ -9,7 +9,7 @@ from os import getcwd
 
 ##### DATABASE LOGIC
 # Reading the data base
-def read_database(database_name: str) -> str:
+def get_database(database_name: str) -> str:
     db_conn.sendall(f"read_database {database_name}".encode())
     db_response = db_conn.recv(8024).decode()
     return db_response
@@ -17,14 +17,14 @@ def read_database(database_name: str) -> str:
 
 ##### PLAYING TEAM LOCAL TACTICS
 # A lobby, when a client is waiting for a game
-def start_lobby(conn: socket, address: str, port: int, name: str, champions: list=[]) -> None:
-    game_lobby.append((conn, address, port, name, champions))
-    console.log(f"'{name}' joined the game lobby. ({len(game_lobby)}/2)")
+def start_lobby(conn: socket, name: str) -> None:
+    lobby.append((conn, name, []))
+    console.log(f"'{name}' joined the game lobby. ({len(lobby)}/2)")
 
-    if len(game_lobby) >= 2:
-        for player in game_lobby:
+    if len(lobby) >= 2:
+        for player in lobby:
             player[0].sendall("lobby_found".encode())
-        console.log(f"Lobby full - {game_lobby}")
+        console.log(f"Lobby full - {[player[1] for player in lobby]}")
         start_game()
 
 
@@ -33,55 +33,43 @@ def start_game() -> None:
     console.log("Starting game...")
 
     for _ in range(2):
-        for selecting_player in game_lobby:
-            selecting_player_conn: socket = selecting_player[0]
-            selecting_player_name: str = selecting_player[3]
-            selecting_player_conn.sendall("choose_champion".encode())
-            for waiting_player in game_lobby:
-                if waiting_player != selecting_player:
+        for curr_player in lobby:
+            curr_player_conn = curr_player[0]
+            curr_player_conn.sendall("choose_champion".encode())
+
+            # For all the other players in the lobby
+            for waiting_player in lobby:
+                if not waiting_player == curr_player:
                     waiting_player_conn = waiting_player[0]
-                    waiting_player_conn.send("waiting".encode())
-            input_champion(selecting_player_conn, selecting_player_name)
-
-    player1_name: str = game_lobby[0][3]
-    player2_name: str = game_lobby[1][3]
-    player1_champions: list = game_lobby[0][4]
-    player2_champions: list = game_lobby[1][4]
-
-    console.log(f"{player1_name} - {player1_champions}")
-    console.log(f"{player2_name} - {player2_champions}")
+                    waiting_player_conn.sendall("waiting".encode())
     
 
-    match = Match(
-        Team([player1_champions[name] for name in player1_name]), Team([player2_champions[name] for name in player2_name]))
-    match.play()
+    # match = Match(
+    #     Team([player1_champions[name] for name in player1_name]), Team([player2_champions[name] for name in player2_name]))
+    # match.play()
 
-# Input champions
-def input_champion(choosing_player_conn: socket, choosing_player_name: str):
-    champions = read_database("champions")
+    for player in lobby:
+        player[0].sendall("match_done".encode())
 
-    while True:
-        champion_selected = choosing_player_conn.recv(1024).decode().lower()
-        player1_champions: list = [champion for champion in game_lobby[0][4]]
-        player2_champions: list = [champion for champion in game_lobby[1][4]]
-        ERROR: str = "[bold red]"
+    console.log("Match done!", style="bold green")
 
-        if champion_selected not in [champion["name"] for champion in champions]:
-            choosing_player_conn.sendall(f"{ERROR}Champion does not exist...".encode())
-        elif champion_selected in player1_champions:
-            choosing_player_conn.sendall(f"{ERROR}Champion has already been chosen...".encode())
-        elif champion_selected in player2_champions:
-            choosing_player_conn.sendall(f"{ERROR}Champion has already been chosen...".encode())
-        else:
-            choosing_player_conn.sendall(f"champion_locked_in".encode())
-            for players in game_lobby:
-                player_name = players[3]
-                player_champions = players[4]
-                if choosing_player_name == player_name:
-                    for champion in champions:
-                        if champion["name"] == champion_selected:
-                            player_champions.append(champion_selected)
-            break
+# Validate a champion. If the champions passes the tests, add the champion to the players champion pool.
+def validate_champion(curr_player: socket, champion_selected: str):
+    champions = get_database("champions")
+    ERROR: str = "[bold red]"
+
+    player1_champions = lobby[0][2]
+    player2_champions = lobby[1][2]
+
+    if champion_selected not in [champion["name"] for champion in champions]:
+        curr_player.sendall(f"{ERROR}Champion does not exist...".encode())
+    elif champion_selected in player1_champions:
+        curr_player.sendall(f"{ERROR}Champion has already been chosen...".encode())
+    elif champion_selected in player2_champions:
+        curr_player.sendall(f"{ERROR}Champion has already been chosen...".encode())
+    else:
+        curr_player[2].append(champions[champion_selected])
+        curr_player.sendall(f"[bold green]Champion {champion_selected.capitalize()} has been added to your team!".encode())
 
 
 ##### SOCKET LOGIC #####
@@ -98,41 +86,19 @@ def accept(sock: str) -> None:
 def read(conn: socket, address: tuple) -> None:
     while True:
         client_input: bytes = conn.recv(1024)
+        console.log(client_input)
         
         if client_input:
-            client_input_decoded: str = client_input.decode()
-            console.log(f"{address} asked for input: '{client_input_decoded}'")
+            client_input_decoded: str = client_input.decode().split(" ", 1)
+            console.log(f"{address} sent: '{client_input_decoded}'")
+            command: str = client_input_decoded[0]
+            args: str = client_input_decoded[1]
 
-            if client_input_decoded.split("_", 1)[0] == "get":
-                match client_input_decoded:
-                    case "get_help_database":
-                        database_content: str = read_database("help")
-                        if database_content != "error":
-                            console.log(f"Sendig help database to {address}")
-                            conn.sendall(database_content.encode())
-                    case "get_champions_database":
-                        database_content: str = read_database("champions")
-                        if database_content != "error":
-                            console.log(f"Sending champions database to {address}.")
-                            conn.sendall(database_content.encode())
-                    case "get_match_history_database":
-                        database_content: str = read_database("match_history")
-                        if database_content != "error":
-                            console.log(f"Sending match history to {address}.")
-                            conn.sendall(database_content.encode())
-
-            elif client_input_decoded.split("_", 1)[0]== "game":
-                player_name: str = client_input_decoded.split("_", 2)[2]
-                method: str = client_input_decoded.split("_", 2)[1]
-                player_address: str = tuple(address)[0]
-                player_port: int = tuple(address)[1]
-
-                # game_start_name
-                if method == "start":
-                    start_lobby(conn, player_address, player_port, player_name)
+            if command in commands:
+                commands[command](args)
             else:
-                print(f"{address} sent an unknown command: '{client_input_decoded}'")
-                conn.sendall("error".encode())
+                conn.sendall("[bold red]Invalid input!".encode())
+
         else:
             console.log(f"Client {address} has connected", style=TXT_DCON)
             conn.close()
@@ -140,6 +106,13 @@ def read(conn: socket, address: tuple) -> None:
 
 
 ##### MAIN #####
+
+commands: dict = {
+    "get_database": get_database,
+    "validate_champion": validate_champion,
+    "start_lobby": start_lobby,
+}
+
 error_text: str = str(["error", "An error occurred loading the data."]).encode()
 cwd: str = getcwd()
 console: Console = Console()
@@ -157,8 +130,8 @@ LISTEN: int = 2
 DB_HOST: str = ""
 DB_PORT: int = 8888
 
-
-game_lobby: list[tuple[socket, str, int, str, list]] = []
+# Players and lobby
+lobby: list = []
 
 if __name__ == "__main__":
     # Set up TCP socket
